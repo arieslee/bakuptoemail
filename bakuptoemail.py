28ccc506e82f43a8d48238e66dfd90340d33df2d
Aries <helloaries@qq.com>
2016-03-02 20:33:23 +0800

    init

---
+++ bakuptoemail.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/3/2 14:28
+# @Author  : Aries (i@iw3c.com)
+# @Site    : http://iw3c.com
+# @File    : bakuptoemail.py
+# @Software: PyCharm
+
+import os,sys,getopt,time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], 'h:s:t:u:p:f:')
+except getopt.GetoptError, err:
+	print str(err)
+	exit()
+
+SMTP_HOST = SMTP_SENDER = SMTP_TO = SMTP_USER = SMTP_PWD = ''
+FILENAME = ''
+CHARSET = 'utf-8'
+for k,v in opts:
+    if k=='-h':
+        SMTP_HOST = v
+    elif k=='-s':
+        SMTP_SENDER = v
+    elif k=='-t':
+        SMTP_TO = v
+    elif k=='-u':
+        SMTP_USER = v
+    elif k=='-p':
+        SMTP_PWD = v
+    elif k=='-f':
+        FILENAME = v
+    #print "k: %s ,v : %s" % (k,v)
+def send_email():
+    #创建一个带附件的实例
+    msg = MIMEMultipart()
+    #构造附件
+    name = os.path.basename(FILENAME)
+    att = MIMEText(open(FILENAME, 'rb').read(), 'base64', CHARSET)
+    att["Content-Type"] = 'application/octet-stream'
+    att["Content-Disposition"] = 'attachment; filename="%s"' % name #这里的filename可以任意写，写什么名字，邮件中显示什么名字
+    msg.attach(att)
+    #加邮件头
+    msg['to'] = SMTP_TO
+    msg['from'] = SMTP_USER
+    msg['subject'] = '[备份]'+time.strftime('%Y-%m-%d',time.localtime())+'网站的备份邮件'
+    #发送邮件
+    try:
+        server = smtplib.SMTP()
+        server.connect(SMTP_HOST)
+        server.login(SMTP_USER,SMTP_PWD)
+        server.sendmail(msg['from'], msg['to'],msg.as_string())
+        server.quit()
+        return True
+    except Exception, e:
+        return False
+
+if __name__ == '__main__':
+    send_email()
---
+++ bakuptoemail.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#author:Aries
+#web:http://iw3c.com
+
+## 备份配置信息 ##
+# 备份名称，用于标记
+BAKUP_NAME="bakup"
+# 备份目录，多个请空格分隔
+BAKUP_SRC="/home/wwwroot/iw3c.com"
+# 备份文件临时存放目录，一般不需要更改
+BAKUP_DIR="/tmp/bakuptoemail"
+# Mysql主机地址
+MYSQL_SERVER="localhost"
+# Mysql用户名
+MYSQL_USER="root"
+# Mysql密码
+MYSQL_PASS="password"
+# Mysql备份数据库，多个请空格分隔
+MYSQL_DBS="iw3c_com_db "
+# 备份文件压缩密码
+BAKUP_FILE_PASSWD="iw3c.com"
+# 邮件SMTP地址
+THESMTP_HOST="smtp.xxx.com"
+# 邮件登录帐号
+THESMTP_USER='aries@xxx.com'
+# 邮件登录密码
+THESMTP_PWD='password'
+# 邮件接收地址
+THESMTP_TO='xxx@xxx.com'
+## 备份配置信息 End ##
+NOW=$(date +"%Y%m%d%H%M%S") #精确到秒，统一秒内上传的文件会被覆盖
+mkdir -p $BAKUP_DIR
+# 备份Mysql
+echo "start dump mysql"
+for db_name in $MYSQL_DBS
+do
+	mysqldump -u $MYSQL_USER -h $MYSQL_SERVER -p$MYSQL_PASS $db_name > "$BAKUP_DIR/$BAKUP_NAME-$db_name.sql"
+done
+echo "dump ok"
+#
+# 打包
+echo "start tar"
+BAKUP_FILENAME="$BAKUP_NAME-$NOW.zip"
+zip -q -r -P $BAKUP_FILE_PASSWD $BAKUP_DIR/$BAKUP_FILENAME $BAKUP_DIR/*.sql $BAKUP_SRC
+echo "tar ok"
+
+# 发送附件
+echo "start send email"
+REAL_FILE=${BAKUP_DIR}/${BAKUP_FILENAME}
+python bakuptoemail.py -h $THESMTP_HOST -s $THESMTP_USER -t $THESMTP_TO -u $THESMTP_USER -p $THESMTP_PWD -f $REAL_FILE
+echo "send ok"
+
+# 清理备份文件
+rm -rf $BAKUP_DIR
+echo "bakup clean done"
